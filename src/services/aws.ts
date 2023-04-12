3d7e0817beea625a31e01ef5bb2d0a10c1bdac89
Victoria Kovalenko <v.kovalenkoooooo@gmail.com>
2023-04-12 13:45:03 +0300

    add public acces to aws

--- src/services/aws.ts
+++ src/services/aws.ts
@@ -4,12 +4,20 @@ import { v4 } from 'uuid';
 dotenv.config();
 
 export const uploadImage = async (compressedImage: string) => {
+  // const config = {
+  //   credentials: {
+  //     accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+  //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+  //   },
+  //   region: process.env.AWS_REGION || '',
+  // };
   const s3 = new AWS.S3();
   const params = {
     Bucket: process.env.AWS_BUCKET_NAME,
     Key: `${v4()}.jpg`,
     Body: Buffer.from(compressedImage, 'base64'),
-    ContentType: 'image/jpg'
+    ContentType: 'image/jpg',
+    ACL: 'public-read'
   };
 
   const s3UploadResult = await s3.upload(params).promise();
